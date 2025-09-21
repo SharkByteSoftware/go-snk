42998bf7d3517784339e79cc3ef69be2ce1be9db
Carlos <98762429+carlos-sandoval-periship@users.noreply.github.com>
2025-09-20 21:46:33 -0500

    added initial stack impl

--- containers/queues/queue.go
+++ containers/queues/queue.go
@@ -1,1 +1,21 @@
 package queues
+
+type Queue[T any] struct{}
+
+func New[T any]() *Queue[T] {
+	return &Queue[T]{}
+}
+
+func (q *Queue[T]) Enqueue(value T) {}
+
+func (q *Queue[T]) Dequeue() (value T, ok bool) {
+	return *new(T), false
+}
+
+func (q *Queue[T]) Peek() (value T, ok bool) {
+	return *new(T), false
+}
+
+func (q *Queue[T]) Values() []T {
+	return []T{}
+}
--- containers/stacks/stack.go
+++ containers/stacks/stack.go
@@ -1,1 +1,44 @@
+// Package stacks contains a stack implementation.
 package stacks
+
+import (
+	"github.com/SharkByteSoftware/go-snk/containers/lists"
+)
+
+type Stack[T comparable] struct {
+	members *lists.List[T]
+}
+
+func New[T comparable]() *Stack[T] {
+	return &Stack[T]{
+		members: lists.New[T](),
+	}
+}
+
+func (s *Stack[T]) Push(value T) {
+	s.members.PushFront(value)
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+	if s.members.IsEmpty() {
+		return *new(T), false
+	}
+
+	return s.members.Remove(s.members.Front()), true
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	if s.members.IsEmpty() {
+		return *new(T), false
+	}
+
+	return s.members.Front().Value, true
+}
+
+func (s *Stack[T]) Size() int {
+	return s.members.Len()
+}
+
+func (s *Stack[T]) Values() []T {
+	return s.members.Values()
+}

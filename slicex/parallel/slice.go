1761e143f9f5b07461164ef8ad78f2a3055bd94b
Carlos <98762429+carlos-sandoval-periship@users.noreply.github.com>
2025-09-30 19:59:37 -0500

    completed parallel slice functions

--- slicex/parallel/slice.go
+++ slicex/parallel/slice.go
@@ -2,6 +2,9 @@ package parallel
 
 import (
 	"sync"
+
+	"github.com/SharkByteSoftware/go-snk/conditional"
+	"github.com/SharkByteSoftware/go-snk/slicex"
 )
 
 // Map transforms a slice to a slice of another type using a mapper function.
@@ -29,21 +32,50 @@ func Map[S ~[]T, T any, R any](slice S, mapper func(item T) R) []R {
 // Apply applies a function to each item in the slice.  The apply function is called
 // in parallel.
 func Apply[S ~[]T, T any](slice S, apply func(item T)) {
-	// TODO: implement
-	panic("implement me")
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(len(slice))
+
+	slicex.Apply(slice, func(item T) {
+		go func() {
+			apply(item)
+
+			waitGroup.Done()
+		}()
+	})
+
+	waitGroup.Wait()
 }
 
 // GroupBy returns a map of slices grouped by a key produced by a predicate function.
 // The predicate is called in parallel, and the results are returned in the order they
 // appear in the slice.
-func GroupBy[S ~[]T, T any, R comparable](slice S, predicate func(item T) R) map[R][]T {
-	// TODO: implement
-	panic("implement me")
+func GroupBy[S ~[]T, T any, R comparable](slice S, predicate func(item T) R) map[R]S {
+	result := make(map[R]S, len(slice))
+
+	keys := Map(slice, func(item T) R {
+		return predicate(item)
+	})
+
+	for idx, item := range slice {
+		result[keys[idx]] = append(result[keys[idx]], item)
+	}
+
+	return result
 }
 
 // Partition splits a slice into two slices based on a predicate.  The predicate is called
 // in parallel, and the results are returned in the order they appear in the slice.
 func Partition[S ~[]T, T any](slice S, predicate func(item T) bool) (S, S) {
-	// TODO: implement
-	panic("implement me")
+	result1 := make(S, 0)
+	result2 := make(S, 0)
+
+	result := Map(slice, func(item T) *S {
+		return conditional.If(predicate(item), &result1, &result2)
+	})
+
+	for idx, item := range slice {
+		*result[idx] = append(*result[idx], item)
+	}
+
+	return result1, result2
 }
--- slicex/parallel/slice_test.go
+++ slicex/parallel/slice_test.go
@@ -2,13 +2,17 @@ package parallel_test
 
 import (
 	"strconv"
+	"sync/atomic"
 	"testing"
 
+	"github.com/SharkByteSoftware/go-snk/conditional"
 	"github.com/SharkByteSoftware/go-snk/slicex/parallel"
 	"github.com/stretchr/testify/assert"
 )
 
 var numberList = []int{1, 2, 3, 4, 5, 333, 256}
+var duplicateList = []int{1, 2, 3, 4, 5, 333, 256, 1, 2, 3, 4, 5, 333, 256}
+var allSame = []int{1, 1, 1, 1, 1, 1}
 
 func TestParallelSlice_Map(t *testing.T) {
 	result := parallel.Map(numberList, func(n int) string { return strconv.Itoa(n) })
@@ -18,4 +22,43 @@ func TestParallelSlice_Map(t *testing.T) {
 	result = parallel.Map([]int{}, func(n int) string { return strconv.Itoa(n) })
 	assert.Equal(t, []string{}, result)
 	assert.Equal(t, 0, cap(result))
+}
+
+func TestParallelSlice_Apply(t *testing.T) {
+	var counter int32
+	parallel.Apply(numberList, func(n int) { atomic.AddInt32(&counter, 1) })
+	assert.Equal(t, int32(7), counter)
+}
+
+func TestParallelSlice_GroupBy(t *testing.T) {
+	result := parallel.GroupBy(numberList, func(item int) int { return item })
+	assert.Len(t, result, len(numberList))
+
+	result = parallel.GroupBy(duplicateList, func(item int) int { return item })
+	assert.Len(t, result, 7)
+
+	stringMap := parallel.GroupBy(duplicateList, func(item int) string {
+		return conditional.If(item%2 == 0, "even", "odd")
+	})
+	assert.Len(t, stringMap, 2)
+	assert.Len(t, stringMap["even"], 6)
+	assert.Len(t, stringMap["odd"], 8)
+}
+
+func TestParallelSlice_Partition(t *testing.T) {
+	r1, r2 := parallel.Partition([]int{}, func(item int) bool { return true })
+	assert.Len(t, r1, 0)
+	assert.Len(t, r2, 0)
+
+	r1, r2 = parallel.Partition(numberList, func(item int) bool { return true })
+	assert.Len(t, r1, 7)
+	assert.Len(t, r2, 0)
+
+	r1, r2 = parallel.Partition(numberList, func(item int) bool { return false })
+	assert.Len(t, r1, 0)
+	assert.Len(t, r2, 7)
+
+	r1, r2 = parallel.Partition(numberList, func(item int) bool { return item%2 == 0 })
+	assert.Len(t, r1, 3)
+	assert.Len(t, r2, 4)
 }

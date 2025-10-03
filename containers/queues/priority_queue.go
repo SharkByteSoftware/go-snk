85bd081d2289de1120137f9fb7287cfb77efb126
michelle-periship <michelle.chen@periship.com>
2025-10-02 20:41:28 -0500

    WIP

--- containers/queues/priority_queue.go
+++ containers/queues/priority_queue.go
@@ -1,42 +1,72 @@
 package queues
 
-type PriorityQueue[T any] struct{}
+import "github.com/SharkByteSoftware/go-snk/slicex"
 
-func NewPriorityQueue[T any](comparator func(prev T, curr T) int) *PriorityQueue[T] {
-	return &PriorityQueue[T]{}
+type Item[T any] struct {
+	value    T
+	index    int
+	priority int
 }
 
-func (q *PriorityQueue[T]) Enqueue(value T) {
-	// TODO: implement
-	panic("implement me")
+type PriorityQueue[T any] struct {
+	items []*Item[T]
 }
 
-func (q *PriorityQueue[T]) Dequeue() (T, bool) {
-	// TODO: implement
-	panic("implement me")
+func (pq *PriorityQueue[T]) Len() int {
+	return len(pq.items)
 }
 
-func (q *PriorityQueue[T]) Peek() (T, bool) {
-	// TODO: implement
-	panic("implement me")
+func (pq *PriorityQueue[T]) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq.items[i].priority > pq.items[j].priority
 }
 
-func (q *PriorityQueue[T]) IsEmpty() bool {
-	// TODO implement me
-	panic("implement me")
+func (pq *PriorityQueue[T]) Swap(i, j int) {
+	pq.items[i], pq.items[j] = pq.items[j], pq.items[i]
+	pq.items[i].index = i
+	pq.items[j].index = j
 }
 
-func (q *PriorityQueue[T]) Size() int {
-	// TODO implement me
-	panic("implement me")
+func NewPriorityQueue[T any](comparator func(prev T, curr T) int) PriorityQueue[T] {
+	return PriorityQueue[T]{}
 }
 
-func (q *PriorityQueue[T]) Clear() {
-	// TODO implement me
-	panic("implement me")
+func (pq *PriorityQueue[T]) Enqueue(value T) {
+	n := pq.Len()
+	item := Item[T]{value: value, index: n}
+	pq.items = append(pq.items, &item)
 }
 
-func (q *PriorityQueue[T]) Values() []T {
-	// TODO implement me
-	panic("implement me")
+func (pq *PriorityQueue[T]) Dequeue() (T, bool) {
+	old := pq
+	n := old.Len()
+	item := old.items[n-1]
+	old.items[n-1] = nil
+	item.index = -1
+	pq.items = old.items[0 : n-1]
+	return item.value, true
+}
+
+func (pq *PriorityQueue[T]) Peek() (T, bool) {
+	if pq.Len() == 0 {
+		return *new(T), false
+	}
+
+	return pq.items[0].value, true
+}
+
+func (pq *PriorityQueue[T]) IsEmpty() bool {
+	return pq.Len() == 0
+}
+
+func (pq *PriorityQueue[T]) Size() int {
+	return pq.Len()
+}
+
+func (pq *PriorityQueue[T]) Clear() {
+	pq = &PriorityQueue[T]{}
+}
+
+func (pq *PriorityQueue[T]) Values() []T {
+	return slicex.Map(pq.items, func(item *Item[T]) T { return item.value })
 }

6e92bb65a2731adc61c3ada46a685d57fb1e5686
carlos-sandoval-periship <carlos.sandoval@periship.com>
2025-11-27 08:20:10 -0600

    fixed misspelled word

--- containers/lists/list_std_test.go
+++ containers/lists/list_std_test.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// These tests are from the std Go library to maintain compatability
+// These tests are from the std Go library to maintain compatibility
 
 package lists
 

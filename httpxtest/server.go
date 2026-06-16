372e37ca8caa5c7141f85eef92aaeb39a5a97b6c
carlos-sandoval-periship <carlos.sandoval@periship.com>
2026-06-16 09:45:38 -0500

    WIP

--- httpxtest/server.go
+++ httpxtest/server.go
@@ -3,24 +3,63 @@ package httpxtest
 import (
 	"net/http"
 	"net/http/httptest"
+	"testing"
+
+	"github.com/SharkByteSoftware/go-snk/conditional"
+	"github.com/SharkByteSoftware/go-snk/jsonx"
 )
 
 type ServerBuilder struct {
+	defaultHandler http.HandlerFunc
 }
 
 func NewServerBuilder() *ServerBuilder {
-	return &ServerBuilder{}
+	return &ServerBuilder{
+		defaultHandler: defaultHandler,
+	}
 }
 
-func (sb *ServerBuilder) Build() *httptest.Server {
+func (sb *ServerBuilder) Build(t testing.TB) *httptest.Server {
+	t.Helper()
+
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		defaultHandler(w, req)
+		sb.defaultHandler(w, req)
 	}))
+
+	t.Cleanup(ts.Close)
 
 	return ts
 }
 
+func (sb *ServerBuilder) WithDefaultHandler(handler http.HandlerFunc) *ServerBuilder {
+	sb.defaultHandler = handler
+
+	return sb
+}
+
+func (sb *ServerBuilder) On(statusCode int, result any) *ServerBuilder {
+
+	sb.defaultHandler = func(w http.ResponseWriter, r *http.Request) {
+		err := jsonx.Encode(w, result)
+		w.WriteHeader(conditional.If(err != nil, http.StatusBadRequest, statusCode))
+	}
+
+	return sb
+}
+
+func (sb *ServerBuilder) OnStr(statusCode int, result any) *ServerBuilder {
+	return sb
+}
+
+func (sb *ServerBuilder) OnGet(route string, statusCode int, result any) *ServerBuilder {
+	return sb
+}
+
+func (sb *ServerBuilder) OnGetStr(route string, statusCode int, result string) *ServerBuilder {
+	return sb
+}
+
 func defaultHandler(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusInternalServerError)
-	_, _ = w.Write([]byte("{}"))
+	_, _ = w.Write([]byte(http.StatusText(http.StatusInternalServerError)))
 }
--- httpxtest/server_test.go
+++ httpxtest/server_test.go
@@ -2,21 +2,64 @@ package httpxtest_test
 
 import (
 	"context"
+	"net/http"
 	"testing"
 
 	"github.com/SharkByteSoftware/go-snk/httpx"
 	"github.com/SharkByteSoftware/go-snk/httpxtest"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+type myStruct struct {
+	Name string `json:"name"`
+}
+
+const myStructReturn = `{"name":"test"}`
+
 func TestServerBuilder_DefaultHandler(t *testing.T) {
 	sb := httpxtest.NewServerBuilder()
-	ts := sb.Build()
+	ts := sb.Build(t)
 
 	require.NotNil(t, ts)
-	require.NotNil(t, ts.URL)
+	require.NotEmpty(t, ts.URL)
 
 	result, err := httpx.Get[string](context.Background(), ts.URL)
 	require.Error(t, err)
 	require.Nil(t, result)
+}
+
+func TestServerBuilder_WithDefaultHandler(t *testing.T) {
+	wasCalled := false
+
+	ts := httpxtest.NewServerBuilder().
+		WithDefaultHandler(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(myStructReturn))
+			wasCalled = true
+		}).
+		Build(t)
+
+	require.NotNil(t, ts)
+	require.NotEmpty(t, ts.URL)
+
+	result, err := httpx.Get[myStruct](context.Background(), ts.URL)
+	require.NoError(t, err)
+	require.NotNil(t, result)
+	require.True(t, wasCalled)
+	assert.Equal(t, "test", result.Result.Name)
+}
+
+func TestServerBuilder_HowToUseIt(t *testing.T) {
+	ts := httpxtest.NewServerBuilder().
+		WithDefaultHandler(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(myStructReturn))
+		}).
+		OnGet("/v1/horton", http.StatusOK, myStruct{Name: "Horton"}).
+		OnGetStr("/v1/name", http.StatusOK, myStructReturn).
+		Build(t)
+
+	require.NotNil(t, ts)
+	require.NotEmpty(t, ts.URL)
 }
